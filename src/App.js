9e40483d90711a73b13d8af929c491d8f8e939f8
adelbajtikov <ba12139@auca.kg>
2024-11-14 10:46:03 +0600

    +css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import { ProductCard } from "./components/ProductCard";
+
+const products = [
+  {
+    id: 0,
+    category: "Category 1",
+    name: "Product 1",
+    price: 100,
+    img: "https://static.vecteezy.com/system/resources/previews/021/608/795/non_2x/chatgpt-logo-chat-gpt-icon-on-green-background-free-vector.jpg",
+  },
+  {
+    id: 1,
+    category: "Category 2",
+    name: "Product 2",
+    price: 300,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-vxiLY7Gt-gd3yaWynZV75kQWjqVK2YRLNg&s",
+  },
+]
 function App() {
   return (
-    <div className="App">
-      <ProductCard/>
+    <div className="products-wrapper">
+      {products.map((product)=>(
+        <ProductCard data={product} id={1}/>
+      ))}
+
     </div>
   );
 }
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,7 +1,13 @@
-export function ProductCard(){
+import "./index.css"
+export function ProductCard(props){
+    const { img, category,name,price}=props.data;
+
     return(
-        <div>
-            <h1>product card</h1>
+        <div className="product-card">
+            <img width = {200} src={img} alt="" />
+            <span>{category}</span>
+            <h4>{name}</h4>
+            <span>{price}</span>
         </div>
-)
+    );
 }
