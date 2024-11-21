6e747b9285588f465eaff6243b73898c56b3f64e
adelbajtikov <ba12139@auca.kg>
2024-11-21 10:56:12 +0600

    design

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -4,10 +4,15 @@ export function ProductCard(props){
 
     return(
         <div className="product-card">
-            <img width = {200} src={img} alt="" />
-            <span>{category}</span>
-            <h4>{name}</h4>
-            <span>{price}</span>
+            <div className="container_upper">
+                <img className="product-card__image" width = {200} src={img} alt="" />
+                <span className="cat">{category}</span>
+                <h4>{name}</h4>
+            </div>
+            <div className="container_lower">
+                <span><p>${price}</p></span>
+                <button>Add to cart</button>
+            </div>
         </div>
     );
 }
